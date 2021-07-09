2517ef7c69542df74c3757b78d1beb6446aee69d
Surbhi Palande <csurbhi@gmail.com>
2021-07-09 00:28:25 +0000

    Fixes in the test code.
    When you want to write to alternate blocks, we need to lseek to the
    appropriate block.
    
    Signed-off-by: Surbhi Palande <csurbhi@gmail.com>

--- write_zones.c
+++ write_zones.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define NR_ZONES 2
+#define NR_ZONES 1
 #define NR_BLKS_IN_ZONE 65536
 #define BLKSZ 4096
 
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 {
 	char buff[BLKSZ];
 	int fd, i, j, k, val, ret;
+	unsigned int offset = 0;
 
 	fd = open("/dev/dm-0", O_RDWR);
 	if (fd < 0) {
@@ -48,7 +49,7 @@ int main(int argc, char *argv[])
 			}
 			for(k=0; k<BLKSZ; k++) {
 				if (buff[k] != 1) {
-					perror("\n Read error! 1");
+					printf("\n Read error! 1");
 					printf("\n k: %d buff[k]: %d", k, buff[k]);
 					return errno;
 				}
@@ -57,25 +58,42 @@ int main(int argc, char *argv[])
 	}
 
 	printf("\n Written two zones of blks, and read them back! About to overwrite....");
+	close(fd);
+
+	sync();
+
+	fd = open("/dev/dm-0", O_RDWR);
+	if (fd < 0) {
+		perror("\n Could not create file because: ");
+		printf("\n");
+		return errno;
+	}
+
+
+	lseek(fd, 0, SEEK_SET);
+
+
 	for(i=0; i<BLKSZ; i++) {
 		buff[i] = 2;
 	}
 	
 
-	lseek(fd, 0, SEEK_SET);
-
 	for(i=0; i<NR_ZONES; i++) {
 		for(j=0; j<NR_BLKS_IN_ZONE; j=j+2) {
+			offset = 4096 * j;
+			lseek(fd, offset, SEEK_SET);
 			ret = write(fd, buff, BLKSZ);
 			if (ret < 0) {
 				perror("\n Could not write to file because: ");
 				printf("\n");
 				return errno;
 			}
+			//printf("\n written 2 at j: %d offset: %d", j, offset);
 		}
 	}
 
 	lseek(fd, 0, SEEK_SET);
+
 	for(i=0; i<NR_ZONES; i++) {
 		for(j=0; j<NR_BLKS_IN_ZONE; j++) {
 			ret = read(fd, buff, BLKSZ);
@@ -92,7 +110,7 @@ int main(int argc, char *argv[])
 				if (buff[k] != val) {
 					printf("\n Expected val: %d and found: %d for j: %d, k: %d", val, buff[k], j, k);
 					printf("\n");
-					return errno;
+					continue;
 				}
 			}
 		}
