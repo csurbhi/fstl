ba0aa206756fafd6416cc4a1d5d371b1d7dd5ff2
Surbhi Palande <csurbhi@gmail.com>
2023-04-07 13:15:33 -0700

    Verify that the create and written extents are same
    The bash code - checks if the created and written pbas
    match in the extents. The pba used is the pba that will be
    overwritten by the pba in the gc frontier.
    If we run this on the pba that actually gets written to,
    the length should match, but there should not be any gaps.
    
    Example:
    surbhi@sharada:~/github/fstl$ grep "write_gc_extent" dmesg | tr -s  " " | cut -d " " -f 12,15 | sed -s 's/ /, /' > pba-len-final
    surbhi@sharada:~/github/fstl$ ./find-gap.sh ./pba-len-final
    Total len: 522136
    Total diff: 0
    
    Signed-off-by: Surbhi Palande <csurbhi@gmail.com>

---
+++ find-gap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#grep "Copied" dmesg | tr -s " " | cut -d " " -f 14,16 | sed 's/)$//' | sed 's/ /, /' > pba-len-create
+
+filen=$1
+lines=`cat $filen | wc -l`
+totallen=0
+sum=0
+prevpba=0
+prevlen=0
+totaldiff=0
+totallen=0
+for (( i=1; i<=$lines; i++ ))
+do
+	sum=`expr $prevpba + $prevlen`
+	fields=`tail +$i $filen | head -1`
+	pba=`echo $fields|tr -s " " | cut -d "," -f 1`
+	len=`echo $fields|tr -s " " | cut -d "," -f 2`
+	prevpba=$pba
+	prevlen=$len
+	totallen=`expr $len + $totallen`
+	if [[ $i -gt 1 ]]
+	then
+		if [[ $sum -eq $pba ]]
+		then
+			continue;
+		else
+			diff=`expr $pba - $sum`
+			totaldiff=`expr $totaldiff + $diff`
+			echo "Gap found at entry nr $i $pba $len. Sum: $sum Diff: $diff Prev pba: $prevpba Prev len: $prevlen"
+		fi
+	fi
+	
+done
+echo "Total len: $totallen"
+echo "Total diff: $totaldiff"
